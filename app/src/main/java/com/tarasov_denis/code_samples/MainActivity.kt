0c905b67ab3f50a2241163f987b654fb61a60b3c
nibelung-dt <97225915+nibelung-dt@users.noreply.github.com>
2023-02-22 21:02:20 +0300

    fragment created

--- app/src/main/java/com/tarasov_denis/code_samples/MainActivity.kt
+++ app/src/main/java/com/tarasov_denis/code_samples/MainActivity.kt
@@ -3,9 +3,21 @@ package com.tarasov_denis.code_samples
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //val fragment = supportFragmentManager.findFragmentById(R.id.fragment_container_view)
+        val fragment = TestFragment()
+       // fragment.beginT
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fragment_container_view, fragment)
+            commit()
+        }
+               //fragment_container_view, R.id.test_fragment)
+           //.commit()
+
     }
 }
